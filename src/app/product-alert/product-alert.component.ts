d8fec1a21f15f32f67d2a32d9fe9d49b76220f6c
sonejidivesh <sonejikaran@gmail.com>
2023-02-27 23:13:41 +0000

    3 files modified, 5 added

---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css'],
+})
+export class ProductAlertComponent {
+  //from parent, ProductListComponent
+  @Input() product!: Product;
+
+
+  //to parent
+  @Output() notify = new EventEmitter();
+
+  
+}
